ce74691a07c875c80d12c733486be86b3e4d755e
Justin Walsh <contact.me@thejustinwalsh.com>
2024-05-26 21:19:43 -0400

    feat: example background tilemap

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,7 +1,6 @@
 export const BACKGROUND_COLOR = 0x000000;
 
-export const ASPECT_RATIO = 9 / 19.5;
-export const HEIGHT = 560;
-export const WIDTH = ASPECT_RATIO * HEIGHT;
+export const HEIGHT = 540;
+export const WIDTH = 270;
 
 export const MOVEMENT_SPEED = 6;
--- src/routes/Game.tsx
+++ src/routes/Game.tsx
@@ -2,6 +2,7 @@ import {Container} from '@pixi/react';
 
 import BulletRunner from '../components/BulletRunner';
 import Player from '../components/Player';
+import TileMap from '../components/TileMap';
 import {useGameState} from '../hooks/useGameState';
 
 export default function Game() {
@@ -9,6 +10,7 @@ export default function Game() {
 
   return (
     <Container>
+      <TileMap tileMap="/data/level-1.json" />
       {actions.map('players', (player, index) => (
         <Player key={index} index={index} atlas="/assets/ships/atlas.json" texture={player.texture ?? 'ship-1'} />
       ))}
